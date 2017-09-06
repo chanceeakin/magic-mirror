b19e89c8c3c9d370ede05748bfaf696b66194934
Chance Eakin <chance.eakin@gmail.com>
2017-09-05 22:34:17 -0500

    created basic web server!

--- web/server/src/main.go
+++ web/server/src/main.go
@@ -1,52 +1,13 @@
 package main
 
 import (
-	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/handler"
-	"log"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world, you beautiful thing.", nil
-			},
-		},
-	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, err := graphql.NewSchema(schemaConfig)
-	if err != nil {
-		log.Fatalf("failed to create new schema, error: %v", err)
-	}
-
-	// Query
-	query := `
-		{
-			hello
-		}
-	`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
-	}
-
-	// create a graphl-go HTTP handler with our previously defined schema
-	// and we also set it to return pretty JSON output
-	h := handler.New(&handler.Config{
-		Schema: &schema,
-		Pretty: true,
-	})
-	fs := http.FileServer(http.Dir("static"))
-
-	// serve a GraphQL endpoint at `/graphql`
-	http.Handle("/graphql", h)
+	fs := http.FileServer(http.Dir("../../client/build/"))
 	http.Handle("/", fs)
-
-	// and serve!
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Listening on 8080")
+	http.ListenAndServe(":8080", nil)
 }
